61fb66116f487634eac63515685dad6ca20f34a4
sahinmaral <sahin.maral@hotmail.com>
2024-02-26 15:28:01 +0300

    Fixed of adding answered question's point again when user navigates at the same question again at SocialMediaAddictiveLevelIdentification screen

--- src/pages/ResultOfAddictiveLevel/ResultOfAddictiveLevel.jsx
+++ src/pages/ResultOfAddictiveLevel/ResultOfAddictiveLevel.jsx
@@ -1,12 +1,34 @@
+import { useCallback } from "react";
 import Container from "../../components/Container/Container";
-import { Text } from "react-native";
+import { BackHandler, Text } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
-function ResultOfAddictiveLevel({ route }) {
+function ResultOfAddictiveLevel({ route, navigation }) {
   const { addictiveLevelPoint } = route.params;
+
+  useFocusEffect(
+    useCallback(() => {
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        function () {
+          console.log("Back pressed");
+          return true;
+        }
+      );
+
+      return () => {
+        backHandler.remove();
+        console.log("Component unmounted");
+      };
+    }, [])
+  );
 
   return (
     <Container customClasses={"justify-center items-center"}>
       <Text className="text-white text-[35px] font-semibold">Sonuç</Text>
+      <Text className="text-white text-[35px] font-semibold">
+        {addictiveLevelPoint}
+      </Text>
     </Container>
   );
 }
