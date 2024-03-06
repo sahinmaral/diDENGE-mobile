cf6debb6de1dfc5b5632c5d97898e8953148bc8f
ygorgun <ygorgun@msn.com>
2024-03-06 16:14:57 +0300

    Added DropdownComponent and applied at ContactForm screen and bugs fixed.

--- src/pages/ContactForm/ContactForm.jsx
+++ src/pages/ContactForm/ContactForm.jsx
@@ -2,50 +2,33 @@ import React, { useState } from 'react';
 import Container from "../../components/Container/Container";
 import { ScrollView, TextInput,View,Text,Pressable } from "react-native";
 import GoBackButton from "../../components/GoBackButton";
-import { Formik } from "formik";
 import DropdownComponent from './DropdownComponent';
 
 function ContactForm({ navigation }) {
    
-
-  const formInitialValues = {
-    nameAndSurname: "",
-    email: "",
-  };
     
     return (
 
-      <Container customClasses="px-2">
+      <Container customClasses="px-4">
         <GoBackButton navigation={navigation} header="İletişime Geçin" />
              <View className="flex-[1]"></View>
-             <Formik
-        initialValues={formInitialValues}
-        onSubmit={(values) => console.log(values)}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
           <View className="px-4 flex-[10]">
-            <View className="gap-4 flex-[10]">
+            <View className="gap-y-4">
               <View className="gap-2">
                 <TextInput
-                  className="text-[16px] pt-2 pb-4 bg-gray-100 text-left pl-5"
+                  className="text-[16px] py-2 bg-gray-100 text-left pl-5"
                   placeholderTextColor="gray"
                   placeholder="Ad soyad"
-                  onChangeText={handleChange("nameAndSurname")}
-                  onBlur={handleBlur("nameAndSurname")}
-                  value={values.nameAndSurname}
                 />
                 <TextInput
-                  className="text-[16px] pt-2 pb-4 bg-gray-100 text-left pl-5"
+                  className="text-[16px]  py-2 bg-gray-100 text-left pl-5"
                   placeholderTextColor="gray"
                   placeholder="E-posta adresi"
-                  onChangeText={handleChange("email")}
-                  onBlur={handleBlur("email")}
-                  value={values.email}
                 />
-                <Text className="text-white text-[15px] font-light">Sorun Tipi</Text>
+                <Text className="text-white text-[15px] font-medium">Sorun Tipi</Text>
                 <DropdownComponent />
                 <TextInput
-                  className="text-[16px] pt-2 pb-4 bg-gray-100 text-left pl-5 pt-10 h-60 "
+                  className="text-[16px] py-2 bg-gray-100 text-left pl-5 h-60 "
                   placeholderTextColor="gray"
                   placeholder="İçerik"
                   multiline={true}
@@ -53,19 +36,17 @@ function ContactForm({ navigation }) {
                 />
               </View>
             </View>
-             <View className="flex-[2]">
+             <View className="flex-[2] py-2">
               <Pressable
-                onPress={handleSubmit}
                 className="items-center justify-center bg-saffronMango rounded-md h-[50px]"
               >
-                <Text className="text-white text-[22px] font-light">
+                <Text className="text-white text-[22px] font-medium">
                   Gönder
                 </Text>
               </Pressable>
             </View>
           </View>
-        )}
-      </Formik>
+
               </Container>
     );
   }
---
+++ src/pages/ContactForm/DropdownComponent.styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  icon: {
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  placeholderStyle: {
+    paddingLeft: 20,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+ containerStyle: { 
+    marginTop: -20 
+},
+});
+
+export default styles;
