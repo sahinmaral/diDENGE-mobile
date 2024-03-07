00d20e4ac37e17d92c3dcd6a565d83943fe77576
sahinmaral <sahin.maral@hotmail.com>
2024-03-07 15:28:50 +0300

    Gathered settings screen options at MenuItem component

--- src/pages/ContactForm/ContactForm.jsx
+++ src/pages/ContactForm/ContactForm.jsx
@@ -1,55 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Container from "../../components/Container/Container";
-import { ScrollView, TextInput,View,Text,Pressable } from "react-native";
+import { ScrollView, TextInput, View, Text, Pressable } from "react-native";
 import GoBackButton from "../../components/GoBackButton";
-import DropdownComponent from './DropdownComponent';
+import DropdownComponent from "./DropdownComponent";
 
 function ContactForm({ navigation }) {
-   
-    
-    return (
-
-      <Container customClasses="px-4">
-        <GoBackButton navigation={navigation} header="İletişime Geçin" />
-             <View className="flex-[1]"></View>
-          <View className="flex-[10]">
-            <View className="gap-y-4">
-              <View className="gap-y-2">
-                <TextInput
-                  className="text-[16px] py-2 bg-gray-100 text-left px-5"
-                  placeholderTextColor="gray"
-                  placeholder="Ad soyad"
-                />
-                <TextInput
-                  className="text-[16px]  py-2 bg-gray-100 text-left px-5"
-                  placeholderTextColor="gray"
-                  placeholder="E-posta adresi"
-                />
-                <Text className="text-white text-[15px] font-medium">Sorun Tipi</Text>
-                <DropdownComponent />
-                <TextInput
-                  textAlignVertical='top'
-                  className="text-[16px] py-2 bg-gray-100 text-left px-5 h-60 "
-                  placeholderTextColor="gray"
-                  placeholder="İçerik"
-                  multiline={true}
-                  numberOfLines={4}
-                />
-              </View>
-            </View>
-             <View className="flex-[2] py-2">
-              <Pressable
-                className="items-center justify-center bg-saffronMango rounded-md h-[50px]"
-              >
-                <Text className="text-white text-[22px] font-medium">
-                  Gönder
-                </Text>
-              </Pressable>
-            </View>
+  return (
+    <Container customClasses="px-4">
+      <GoBackButton navigation={navigation} header="İletişime Geçin" />
+      <View className="flex-[1]"></View>
+      <View className="flex-[10]">
+        <View className="gap-y-4">
+          <View className="gap-y-2">
+            <TextInput
+              className="text-[16px] py-2 bg-gray-100 text-left px-5"
+              placeholderTextColor="gray"
+              placeholder="Ad soyad"
+            />
+            <TextInput
+              className="text-[16px]  py-2 bg-gray-100 text-left px-5"
+              placeholderTextColor="gray"
+              placeholder="E-posta adresi"
+            />
+            <Text className="text-white text-[15px] font-medium">
+              Sorun Tipi
+            </Text>
+            <DropdownComponent />
+            <TextInput
+              textAlignVertical="top"
+              className="text-[16px] py-2 bg-gray-100 text-left px-5 h-60 "
+              placeholderTextColor="gray"
+              placeholder="İçerik"
+              multiline={true}
+              numberOfLines={4}
+            />
           </View>
+        </View>
+        <View className="flex-[2] py-2">
+          <Pressable className="items-center justify-center bg-saffronMango rounded-md h-[50px]">
+            <Text className="text-white text-[22px] font-medium">Gönder</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Container>
+  );
+}
 
-              </Container>
-    );
-  }
-  
-  export default ContactForm;
+export default ContactForm;
--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,8 +1,7 @@
 import Container from "../../components/Container/Container";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { useEffect } from "react";
 import GoBackButton from "../../components/GoBackButton";
-import { AntDesign } from "@expo/vector-icons";
 import MenuItem from "../../components/MenuItem/MenuItem";
 import { useNavigation } from "@react-navigation/native";
 
@@ -17,37 +16,24 @@ function Settings({ updateCurrentScreen }) {
     <Container customClasses="px-4">
       <GoBackButton navigation={navigation} header="Ayarlar" />
       <View className="flex-[6]">
-        <View className="flex flex-row justify-between border-b border-white py-4">
-          <Text className="text-white text-lg">Dil</Text>
-          <View className="flex flex-row gap-4 items-center">
-            <Text className="text-white text-lg">Türkçe</Text>
-            <AntDesign name="right" size={20} color="#FFC857" />
-          </View>
-        </View>
-        <View className="flex flex-row justify-between border-b border-white py-4">
-          <Text className="text-white text-lg">Profilim</Text>
-          <View className="flex flex-row gap-4 items-center">
-            <AntDesign name="right" size={20} color="#FFC857" />
-          </View>
-        </View>
-        <View className="flex flex-row justify-between border-b border-white py-4">
-          <Text className="text-white text-lg">Sesli Bildirim</Text>
-          <View className="flex flex-row gap-4 items-center">
-            <Text className="text-white text-lg">Aktif</Text>
-            <AntDesign name="right" size={20} color="#FFC857" />
-          </View>
-        </View>
-        <View className="flex flex-row justify-between border-b border-white py-4">
-          <Text className="text-white text-lg">Odak Mod</Text>
-          <View className="flex flex-row gap-4 items-center">
-            <Text className="text-white text-lg">Aktif</Text>
-            <AntDesign name="right" size={20} color="#FFC857" />
-          </View>
-        </View>
+        <MenuItem header="Dil" subHeader="Türkçe" onPress={() => {}} />
+        <MenuItem header="Profilim" onPress={() => {}} />
+        <MenuItem
+          header="Sesli Bildirim"
+          subHeader="Aktif"
+          onPress={() => {}}
+        />
+        <MenuItem header="Odak Mod" subHeader="Aktif" onPress={() => {}} />
       </View>
       <View className="flex-[2]">
-        <MenuItem header="İletişime Geçin" />
-        <MenuItem header="Gizlilik Sözleşmesi" />
+        <MenuItem
+          header="İletişime Geçin"
+          onPress={() => navigation.navigate("ContactForm")}
+        />
+        <MenuItem
+          header="Gizlilik Sözleşmesi"
+          onPress={() => navigation.navigate("PrivacyPolicy")}
+        />
       </View>
     </Container>
   );
