b117a088c14938b3b773c9e814432bf2f01aa3fc
sahinmaral <sahin.maral@hotmail.com>
2024-03-17 04:01:23 +0300

    Added react-native-push-notification to project and configured options

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import PushNotification, { Importance } from 'react-native-push-notification';
 
 AppRegistry.registerComponent(appName, () => App);
+
+PushNotification.configure({
+    onRegister: function (token) {
+        // console.log("TOKEN:", token);
+    },
+    onNotification: function (notification) {
+        // console.log('LOCAL NOTIFICATION ==>', notification);
+    },
+    popInitialNotification: true,
+    requestPermissions: Platform.OS === 'ios',
+});
+
+PushNotification.createChannel(
+    {
+        channelId: "1",
+        channelName: "ProcedureChannel",
+        playSound: true,
+        importance: Importance.HIGH,
+        soundName: "default",
+        vibrate: true,
+        vibration: 300,
+    },
+    () => { }
+);
