038769c1d80a22eb6db6d7aec9f8dcb45dca3c62
sahinmaral <sahin.maral@hotmail.com>
2024-03-27 15:42:00 +0300

    Fixed condition when length of Object.values(stats) returns zero getTotalSpentTimeOfSocialMediaApplications function of UsageStatsParser util

--- src/utils/UsageStatsParser.js
+++ src/utils/UsageStatsParser.js
@@ -1,7 +1,10 @@
 const getTotalSpentTimeOfSocialMediaApplications = (stats) => {
-  const values = Object.values(stats).map((stat) => stat.totalTimeInForeground);
+  const values = Object.values(stats);
+  if (values.length === 0) return 0;
 
-  const totalTime = values.reduce((value, curr) => value + curr);
+  const totalTime = values.reduce(
+    (value, curr) => value.totalTimeInForeground + curr.totalTimeInForeground
+  );
   return totalTime;
 };
 
